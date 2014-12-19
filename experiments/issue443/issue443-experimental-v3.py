325b5553a3e78b7818f1deb351f3e3d64ff80bc9
Florian Pommerening <florian.pommerening@unibas.ch>
2014-12-19 21:25:09 +0100

    [issue443-experimental] experiment for experimental-v3

---
+++ experiments/issue443/issue443-experimental-v3.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from downward import suites
+from downward.reports.scatter import ScatterPlotReport
+from relativescatter import RelativeScatterPlotReport
+
+import common_setup
+
+CONFIGS = {
+        'astar_lmcount': [
+            '--search',
+            'astar(lmcount(lm_merged([lm_rhw(),lm_hm(m=1)]),admissible=true,optimal=true),mpd=true)'],
+}
+
+exp = common_setup.IssueExperiment(
+    search_revisions=["issue443-base", "issue443-experimental-v3"],
+    configs=CONFIGS,
+    suite=suites.suite_optimal_with_ipc11(),
+    )
+
+exp.add_comparison_table_step()
+
+exp.add_report(ScatterPlotReport(
+                   attributes=["total_time"],
+                   get_category=lambda r1,r2: r1["domain"],
+              ),
+              outfile="%s-scatter-time.png" % exp.name,
+)
+
+exp.add_report(RelativeScatterPlotReport(
+                   attributes=["total_time"],
+                   get_category=lambda r1,r2: r1["domain"],
+              ),
+              outfile="%s-relative-scatter-time.png" % exp.name,
+)
+
+exp()
