3a8e15011f659d59fa5bf125913151645437755d
Florian Pommerening <florian.pommerening@unibas.ch>
2015-09-21 17:40:33 +0200

    [issue67-hacked-no-lp] script to build cmake and make version

---
+++ build_both.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+set -e
+rm -rf builds
+./build.py -j8
+cd src
+./build_all distclean
+./build_all -j8
+cd ..
+mkdir -p builds/make/bin
+cp -r src/translate builds/make/bin/
+cp src/preprocess/preprocess builds/make/bin/
+cp src/seach/downward-release builds/make/bin/downward
+mv builds/release32 builds/cmake
+if [[ "$TESTME" != "" ]]; then
+    ./fast-downward.py --build cmake benchmarks/sokoban-opt11/p09.pddl --search "astar(blind())"
+    ./fast-downward.py --build make benchmarks/sokoban-opt11/p09.pddl --search "astar(blind())"
+fi
