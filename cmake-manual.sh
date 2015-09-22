55750798c2a03af6a3c4f637c2d57b824554ac20
Florian Pommerening <florian.pommerening@unibas.ch>
2015-09-22 16:38:32 +0200

    [issue67-hacked-no-lp] try new order: plugins < core < main

--- cmake-manual.sh
+++ cmake-manual.sh
@@ -141,52 +141,134 @@ cd builds/$BUILD_DIR/
 # done
 cp -r ../common/.obj .
 
+
+### Original build order from cmake
+# LINK_ORDER="
+# planner
+# abstract_task
+# axioms
+# causal_graph
+# cost_adapted_task
+# countdown_timer
+# delegating_task
+# domain_transition_graph
+# equivalence_relation
+# evaluation_context
+# evaluation_result
+# exact_timer
+# global_operator
+# globals
+# global_state
+# heuristic_cache
+# heuristic
+# int_packer
+# operator_cost
+# option_parser
+# option_parser_util
+# per_state_information
+# plugin
+# priority_queue
+# rng
+# root_task
+# sampling
+# scalar_evaluator
+# search_engine
+# search_node_info
+# search_progress
+# search_space
+# search_statistics
+# segmented_vector
+# state_id
+# state_registry
+# successor_generator
+# task_proxy
+# task_tools
+# timer
+# tracer
+# utilities
+# utilities_hash
+# utilities_windows
+# variable_order_finder
+# open_lists/alternation_open_list
+# open_lists/bucket_open_list
+# open_lists/open_list
+# open_lists/pareto_open_list
+# open_lists/standard_scalar_open_list
+# open_lists/tiebreaking_open_list
+# g_evaluator
+# combining_evaluator
+# max_evaluator
+# pref_evaluator
+# weighted_evaluator
+# sum_evaluator
+# eager_search
+# lazy_search
+# enforced_hill_climbing_search
+# iterated_search
+# lp_internals
+# lp_solver
+# relaxation_heuristic
+# ipc_max_heuristic
+# additive_heuristic
+# blind_search_heuristic
+# cea_heuristic
+# cg_heuristic
+# cg_cache
+# ff_heuristic
+# goal_count_heuristic
+# hm_heuristic
+# lm_cut_heuristic
+# max_heuristic
+# merge_and_shrink/distances
+# merge_and_shrink/factored_transition_system
+# merge_and_shrink/fts_factory
+# merge_and_shrink/heuristic_representation
+# merge_and_shrink/label_equivalence_relation
+# merge_and_shrink/labels
+# merge_and_shrink/merge_and_shrink_heuristic
+# merge_and_shrink/merge_dfp
+# merge_and_shrink/merge_linear
+# merge_and_shrink/merge_strategy
+# merge_and_shrink/shrink_bisimulation
+# merge_and_shrink/shrink_bucket_based
+# merge_and_shrink/shrink_fh
+# merge_and_shrink/shrink_random
+# merge_and_shrink/shrink_strategy
+# merge_and_shrink/transition_system
+# landmarks/exploration
+# landmarks/h_m_landmarks
+# landmarks/lama_ff_synergy
+# landmarks/landmark_cost_assignment
+# landmarks/landmark_count_heuristic
+# landmarks/landmark_factory
+# landmarks/landmark_factory_rpg_exhaust
+# landmarks/landmark_factory_rpg_sasp
+# landmarks/landmark_factory_zhu_givan
+# landmarks/landmark_graph
+# landmarks/landmark_graph_merged
+# landmarks/landmark_status_manager
+# landmarks/util
+# pdbs/canonical_pdbs_heuristic
+# pdbs/dominance_pruner
+# pdbs/match_tree
+# pdbs/max_cliques
+# pdbs/pattern_database
+# pdbs/pattern_generation_edelkamp
+# pdbs/pattern_generation_haslum
+# pdbs/pdb_heuristic
+# pdbs/util
+# pdbs/zero_one_pdbs_heuristic
+# potentials/diverse_potential_heuristics
+# potentials/potential_function
+# potentials/potential_heuristic
+# potentials/potential_max_heuristic
+# potentials/potential_optimizer
+# potentials/sample_based_potential_heuristics
+# potentials/single_potential_heuristics
+# potentials/util
+# "
+
 LINK_ORDER="
-planner
-abstract_task
-axioms
-causal_graph
-cost_adapted_task
-countdown_timer
-delegating_task
-domain_transition_graph
-equivalence_relation
-evaluation_context
-evaluation_result
-exact_timer
-global_operator
-globals
-global_state
-heuristic_cache
-heuristic
-int_packer
-operator_cost
-option_parser
-option_parser_util
-per_state_information
-plugin
-priority_queue
-rng
-root_task
-sampling
-scalar_evaluator
-search_engine
-search_node_info
-search_progress
-search_space
-search_statistics
-segmented_vector
-state_id
-state_registry
-successor_generator
-task_proxy
-task_tools
-timer
-tracer
-utilities
-utilities_hash
-utilities_windows
-variable_order_finder
 open_lists/alternation_open_list
 open_lists/bucket_open_list
 open_lists/open_list
@@ -264,6 +346,51 @@ potentials/potential_optimizer
 potentials/sample_based_potential_heuristics
 potentials/single_potential_heuristics
 potentials/util
+abstract_task
+axioms
+causal_graph
+cost_adapted_task
+countdown_timer
+delegating_task
+domain_transition_graph
+equivalence_relation
+evaluation_context
+evaluation_result
+exact_timer
+global_operator
+globals
+global_state
+heuristic_cache
+heuristic
+int_packer
+operator_cost
+option_parser
+option_parser_util
+per_state_information
+plugin
+priority_queue
+rng
+root_task
+sampling
+scalar_evaluator
+search_engine
+search_node_info
+search_progress
+search_space
+search_statistics
+segmented_vector
+state_id
+state_registry
+successor_generator
+task_proxy
+task_tools
+timer
+tracer
+utilities
+utilities_hash
+utilities_windows
+variable_order_finder
+planner
 "
 
 OBJECTS=()
