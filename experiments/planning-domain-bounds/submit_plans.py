16e88411ff85f639976d622156fb50b964e1532a
Florian Pommerening <florian.pommerening@unibas.ch>
2017-05-22 14:32:27 +0200

    [planning-domain-bounds] script to submit plans of an ApiExperiment

---
+++ experiments/planning-domain-bounds/submit_plans.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+
+from lab.tools import Properties
+
+import planning_domains_api as api
+
+import os, sys
+
+
+def submit_plan(pid, plan_path):
+    with open(plan_path) as f:
+        api.submit_plan(pid, f.read())
+
+def submit_plans(experiment_name):
+    data_dir = os.path.join('data', experiment_name)
+    runs = Properties(os.path.join(data_dir + '-eval', 'properties'))
+    for run in runs.values():
+        pid = run['api_problem_id']
+        run_dir = run['run_dir']
+        last_plan = len(run.get('cost_all', 0))
+        if last_plan:
+            last_plan_path = os.path.join(data_dir, run_dir, 'sas_plan.%d' % last_plan)
+            print pid, last_plan_path
+            submit_plan(int(pid), last_plan_path)
+
+
+if __name__ == "__main__":
+    submit_plans(sys.argv[1])
