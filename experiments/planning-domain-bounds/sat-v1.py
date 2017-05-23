ca0f907e671d7e1fa8f1eb344f08123d04bc7820
Florian Pommerening <florian.pommerening@unibas.ch>
2017-05-24 00:44:52 +0200

    [planning-domain-bounds] update experiment for new lab version

--- experiments/planning-domain-bounds/sat-v1.py
+++ experiments/planning-domain-bounds/sat-v1.py
@@ -3,61 +3,25 @@
 import os
 
 from downward.experiment import FastDownwardExperiment
+from downward.suites import Problem
 from lab.environments import LocalEnvironment, MaiaEnvironment
 
 from common_setup import is_test_run, get_repo_base, get_data_dir
 
 import planning_domains_api as api
 
-
-class ApiProblem(object):
-    def __init__(self, problem):
-        problem = api.localize(problem)
-        self.benchmarks_dir = ''
-        self.domain = problem['domain']
-        if not self.domain.endswith('.pddl'):
-            self.domain += '.pddl'
-        self.problem = problem['problem']
-        if not self.problem.endswith('.pddl'):
-            self.problem += '.pddl'
-        self.problem_file = problem['problem_path']
-        if not self.problem_file.endswith('.pddl'):
-            self.problem_file += '.pddl'
-        self.domain_file = problem['domain_path']
-        if not self.domain_file.endswith('.pddl'):
-            self.domain_file += '.pddl'
-        self.api_problem_id = problem['problem_id']
-
-    def __str__(self):
-        return '%s:%s' % (self.domain, self.problem)
-
-    def __repr__(self):
-        return '<Problem %s:%s>' % (self.domain, self.problem)
-
-    def __hash__(self):
-        return hash((self.domain_file, self.problem_file))
-
-    def __cmp__(self, other):
-        return cmp((self.domain_file, self.problem_file), (other.domain_file, other.problem_file))
-
-
-class ApiExperiment(FastDownwardExperiment):
-    def _get_tasks(self):
-        return [ApiProblem(p) for p in self.api_suite]
-
-    def set_api_suite(self, api_suite):
-        self.api_suite = api_suite
-
-    def _add_runs(self):
-        FastDownwardExperiment._add_runs(self)
-        for run in self.runs:
-            run.set_property('api_problem_id', run.task.api_problem_id)
+def api_to_lab_problem(problem):
+    problem = api.localize(problem)
+    return Problem(problem['domain'], problem['problem'],
+        domain_file=problem['domain_path'],
+        problem_file=problem['problem_path'],
+        properties={'api_problem_id': problem['problem_id']})
 
 REVISION = '08bff46b616b'
 
 if is_test_run():
     ENVIRONMENT = LocalEnvironment(processes=1)
-    SUITE = [api.get_problem(210)]
+    SUITE = [api_to_lab_problem(api.get_problem(210))]
 else:
     ENVIRONMENT = MaiaEnvironment(priority=-100, email="florian.pommerening@unibas.ch")
     SUITE = []
@@ -66,11 +30,12 @@ else:
             lb = problem.get('lower_bound') or 0
             ub = problem.get('upper_bound') or float('inf')
             if lb < ub:
-                SUITE.append(problem)
+                SUITE.append(api_to_lab_problem(problem))
+    print "Found %d problems with open bounds" % len(SUITE)
 
-exp = ApiExperiment(path=get_data_dir(), environment=ENVIRONMENT)
+exp = FastDownwardExperiment(path=get_data_dir(), environment=ENVIRONMENT)
 
-exp.set_api_suite(SUITE)
+exp.add_suite('', SUITE)
 
 exp.add_algorithm(
     'lama-24h',
