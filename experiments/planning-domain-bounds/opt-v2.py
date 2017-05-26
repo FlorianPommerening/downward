cd13160252fb3ea4384bd1eca7758752bf8b0691
Florian Pommerening <florian.pommerening@unibas.ch>
2017-05-26 16:00:41 +0200

    [planning-domain-bounds] exclude domains that require unsupported features

--- experiments/planning-domain-bounds/opt-v2.py
+++ experiments/planning-domain-bounds/opt-v2.py
@@ -26,6 +26,24 @@ def api_to_lab_problem(problem):
         })
 
 REVISION = '08bff46b616b'
+# exclude domains with unsupported features
+BAD_DOMAINS = [
+    61,  # airport-adl
+    110, # assembly
+    123, # briefcaseworld
+    26,  # cavediving
+    115, # citycar
+    117, # citycar
+    99,  # miconic
+    114, # miconic
+    92,  # optical-telegraphs
+    106, # philosophers
+    41,  # psr-large
+    48,  # psr-middle
+    94,  # trucks
+         # elevators (some tasks are ok)
+         # openstacks (some tasks are ok)
+]
 
 if False: #is_test_run():
     ENVIRONMENT = LocalEnvironment(processes=1)
@@ -33,7 +51,8 @@ if False: #is_test_run():
 else:
     ENVIRONMENT = MaiaEnvironment(priority=-100, email="florian.pommerening@unibas.ch")
     SUITE = [api_to_lab_problem(problem)
-        for problem in api.simple_query('/classical/problems/search?openbounds=1')]
+        for problem in api.simple_query('/classical/problems/search?openbounds=1')
+        if int(problem["domain_id"]) not in BAD_DOMAINS]
     print "Found %d problems with open bounds" % len(SUITE)
 
 exp = FastDownwardExperiment(path=get_data_dir(), environment=ENVIRONMENT)
