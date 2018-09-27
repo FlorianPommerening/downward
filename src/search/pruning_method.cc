e97e75ec299a67fe9446b67ea2171600fe8adad3
Florian Pommerening <florian.pommerening@unibas.ch>
2018-09-27 16:26:08 +0200

    [staterepresentation] remove overload for packed states in pruning methods

--- src/search/pruning_method.cc
+++ src/search/pruning_method.cc
@@ -16,17 +16,6 @@ void PruningMethod::initialize(const shared_ptr<AbstractTask> &task_) {
     task = task_;
 }
 
-// TODO remove this overload once the search uses the task interface.
-void PruningMethod::prune_operators(const GlobalState &global_state,
-                                    vector<OperatorID> &op_ids) {
-    assert(task);
-    /* Note that if the pruning method would use a different task than
-       the search, we would have to convert the state before using it. */
-    State state = global_state.unpack();
-
-    prune_operators(state, op_ids);
-}
-
 static PluginTypePlugin<PruningMethod> _type_plugin(
     "PruningMethod",
     "Prune or reorder applicable operators.");
