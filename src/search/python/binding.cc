21d59bd422e11c7cfb15dde2bf12dd9461b8d542
Florian Pommerening <florian.pommerening@unibas.ch>
2025-07-29 00:03:46 +0200

    register list types

--- src/search/python/binding.cc
+++ src/search/python/binding.cc
@@ -8,6 +8,7 @@ using namespace std;
 NB_MODULE(pydownward, m) {
     unordered_set<string> manually_specified = bind_basic_classes(m);
     bind_feature_classes(m, manually_specified);
+    bind_list_classes(m);
     bind_enums(m);
     bind_features(m);
 }
